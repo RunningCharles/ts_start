283c48cbd380814a4ee902b11c20a5ebddec0423
leogcchen <leogcchen@tencent.com>
2023-12-07 10:30:43 +0800

    init project

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+//
+//
+//      ┏┛ ┻━━━━━┛ ┻┓
+//      ┃　　　　　　 ┃
+//      ┃　　　━　　　┃
+//      ┃　┳┛　  ┗┳　┃
+//      ┃　　　　　　 ┃
+//      ┃　　　┻　　　┃
+//      ┃　　　　　　 ┃
+//      ┗━┓　　　┏━━━┛
+//        ┃　　　┃   神兽保佑
+//        ┃　　　┃   代码无BUG！
+//        ┃　　　┗━━━━━━━━━┓
+//        ┃　　　　　　　    ┣┓
+//        ┃　　　　         ┏┛
+//        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+//          ┃ ┫ ┫   ┃ ┫ ┫
+//          ┗━┻━┛   ┗━┻━┛
+//
+//  Created by CharlesChen on 2023/12/07.
+//  Copyright © 2020年 Tencent. All rights reserved.
+
+
+(async function () {
+  console.info('>>> begin');
+  // TODO
+  console.info('>>> end');
+})();
