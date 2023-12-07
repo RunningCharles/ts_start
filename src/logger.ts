7f39412843f9249a09211e288f84f09d20ebffe7
leogcchen <leogcchen@tencent.com>
2023-12-07 10:50:45 +0800

    add log4js

--- src/logger.ts
+++ src/logger.ts
@@ -20,10 +20,7 @@
 //  Created by CharlesChen on 2023/12/07.
 //  Copyright © 2020年 Tencent. All rights reserved.
 
-import { logger } from './logger';
+import log4js from 'log4js'
 
-(async function () {
-  logger.info('>>> begin');
-  // TODO
-  logger.info('>>> end');
-})();
+export const logger = log4js.getLogger();
+logger.level = "debug";
